693c5fc0fd45541ff80f73d8848cca1ad1d41761
feelonewong <feelonewong@gmail.com>
2020-06-01 18:00:56 +0800

    import some package

---
+++ src/views/Login/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="login">
+      <el-button type="primary">123</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+#login{
+    background:#344a5f;
+    height: 100vh;
+}
+</style>
